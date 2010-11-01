a86241fe436d78c8056b4e736ddfce3e981d2b6d
Kip Cole <kip@Kip-macbook.local>
2010-11-01 23:39:55 +0800

    Remove DS_Store files

--- lib/ar_to_chart.rb
+++ lib/ar_to_chart.rb
@@ -1,15 +1,15 @@
-require File.dirname(__FILE__) + '/charting/highcharts.rb'
-require File.dirname(__FILE__) + '/charting/sparklines.rb'
-require File.dirname(__FILE__) + '/charting/period.rb'
-require File.dirname(__FILE__) + '/charting/highcharts/base'
-require File.dirname(__FILE__) + '/charting/highcharts/area'
-require File.dirname(__FILE__) + '/charting/highcharts/pie'
-require File.dirname(__FILE__) + '/charting/highcharts/funnel'
+require File.dirname(__FILE__) + '/ar_to_chart/charting/highcharts.rb'
+require File.dirname(__FILE__) + '/ar_to_chart/charting/sparklines.rb'
+require File.dirname(__FILE__) + '/ar_to_chart/charting/period.rb'
+require File.dirname(__FILE__) + '/ar_to_chart/charting/highcharts/base'
+require File.dirname(__FILE__) + '/ar_to_chart/charting/highcharts/area'
+require File.dirname(__FILE__) + '/ar_to_chart/charting/highcharts/pie'
+require File.dirname(__FILE__) + '/ar_to_chart/charting/highcharts/funnel'
 
-require File.dirname(__FILE__) + '/active_record_array.rb'
+require File.dirname(__FILE__) + '/ar_to_chart/active_record_array.rb'
 Array.send :include, Charting::ActiveRecordArray
 
-require File.dirname(__FILE__) + '/charting/transforms.rb'
+require File.dirname(__FILE__) + '/ar_to_chart/charting/transforms.rb'
 ActiveRecord::Base.send :include, Charting::Transforms
 
 I18n.load_path << Dir[ File.join(File.dirname(__FILE__), 'locale', '*.{rb,yml}') ]
